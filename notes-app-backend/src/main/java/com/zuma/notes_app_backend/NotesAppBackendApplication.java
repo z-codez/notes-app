56137aa675c0ba523fab7099f76001ca2c2a914f
dci-student <137389454+z-codez@users.noreply.github.com>
2026-01-09 11:48:23 +0100

    Enabled environment variables for db connection

--- notes-app-backend/src/main/java/com/zuma/notes_app_backend/NotesAppBackendApplication.java
+++ notes-app-backend/src/main/java/com/zuma/notes_app_backend/NotesAppBackendApplication.java
@@ -1,5 +1,6 @@
 package com.zuma.notes_app_backend;
 
+//import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NotesAppBackendApplication {
 
 	public static void main(String[] args) {
+		// Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(NotesAppBackendApplication.class, args);
 	}
 
