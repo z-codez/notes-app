f529b1645ba7ca6cbf6cb6e331ed8375a29a22e5
dci-student <137389454+z-codez@users.noreply.github.com>
2026-01-19 15:45:16 +0100

    Added a get all Notes function to this hook

--- notes-app-mobile/hooks/notes/use-notes-storage.ts
+++ notes-app-mobile/hooks/notes/use-notes-storage.ts
@@ -1,11 +1,36 @@
-import { addNote as postNote } from "@/services/api/notes-api";
-import type { Note } from "@/services/api/types/note";
+import { useEffect, useState } from "react";
+import { Note, getNotes, getNote , addNote, updateNote} from "@/services/local-storage/sql-lite/notesDb";
 import { useLocalSearchParams } from "expo-router";
 import { getFormattedDate } from "@/utils/date";
 
 // TODO: Finish this hook
 export function useNotesStorageGetAll() {
+    // Tip: useState to trigger re-renders when state changes
+    const [notes, setNotes] = useState<Note[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState("");
 
+    
+    async function fetchNotes() {
+        try {
+        // Tip: await can only be used inside async functions
+        const fetchedNotes = await getNotes();
+        notes.push(...fetchedNotes);
+        
+        } catch (err) {
+            if(err instanceof Error) {
+                setError(err.message);
+            } else {
+                setError("Unknown error just occurred");
+            }
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    fetchNotes();
+
+    return {notes, loading, error};
 
 }
 
@@ -14,7 +39,7 @@ export function useNotesStorageGetOne() {
 }
 
 export function useNotesStoragePost() {
-
+    
 }
 
 export function useNotesStorageDelete() {
