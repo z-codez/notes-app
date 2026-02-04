0ee34c65ac3658debebc11f3e23dcb8f81fd8b18
dci-student <137389454+z-codez@users.noreply.github.com>
2026-02-04 10:31:50 +0100

    refactor: reduced the length of subtitle generated

--- notes-app-mobile/utils/string-formater.ts
+++ notes-app-mobile/utils/string-formater.ts
@@ -1,7 +1,7 @@
 export function subtitleGenerator(text: string) {
     
-    if (text.length > 30) {
-        return text.slice(0, 30) + '...';
+    if (text.length > 25) {
+        return text.slice(0, 25) + '...';
     } else {
         return text;
     }
